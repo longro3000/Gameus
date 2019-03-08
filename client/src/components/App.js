0cb39a881138df5367436b4ae37382edd3a3dcd9
longro3000 <longro3000@gmail.com>
2019-03-08 18:16:26 +0200

    first commit

---
+++ client/src/components/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Router, Route, Switch } from 'react-router-dom';
+
+import Header from './Header';
+import history from '../history'
+
+export default class App extends React.Component {
+    render() {
+        return (
+          <div>
+          <Router history={history}>
+              <div>
+                <Header />
+                <Switch>
+
+                </Switch>
+              </div>
+          </Router>
+          </div>
+      );
+    }
+}
+
---
+++ client/src/components/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return(
+        <div>HEADER HEREEEEEEE</div>
+    );
+}
+
+export const Header;
